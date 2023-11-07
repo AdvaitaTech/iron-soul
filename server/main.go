424afac33ecea3ec19b822a22ea8c07aac853bc9
Vikram Thyagarajan <vikramthyagarajan92@gmail.com>
2023-11-07 19:39:44 +0530

    feat: added initial server app

---
+++ server/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"os"
+
+	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+)
+
+func loadEnv() {
+	env := os.Getenv("ENV")
+	if env != "" {
+		env = "development"
+	}
+	var file = ".env.development"
+	switch env {
+	case "development":
+		file = ".env.development"
+	case "production":
+		file = ".env.production"
+	case "test":
+		file = ".env.test"
+	}
+
+	godotenv.Load("../" + file)
+}
+
+func main() {
+	loadEnv()
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+	r.Run()
+}
---
+++ server/scripts/schema.sql
@@ -0,0 +1,96 @@
+\set ON_ERROR_STOP true
+SET statement_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SET check_function_bodies = false;
+SET client_min_messages = warning;
+
+-- ENUMS
+CREATE TYPE muscle AS ENUM ('abdominals','hamstrings','adductors','quadriceps','biceps','shoulders','chest','middle back','calves','glutes','lower back','lats','triceps','traps','forearms','neck','abductors');
+CREATE TYPE forceType AS ENUM ('pull','push','static');
+CREATE TYPE levelType AS ENUM ('beginner','intermediate','expert');
+CREATE TYPE mechanicType AS ENUM ('compound','isolation');
+CREATE TYPE equipmentType AS ENUM ('body only','machine','other','foam roll','kettlebells','dumbbell','cable','barbell','bands','medicine ball','exercise ball','e-z curl bar');
+CREATE TYPE categoryType AS ENUM ('strength','stretching','plyometrics','strongman','powerlifting','cardio','olympic weightlifting');
+
+-- Create Exercises Table
+DROP TABLE IF EXISTS exercises;
+CREATE TABLE exercises (
+  id UUID,
+  name Text NOT NULL,
+  aliases Text[],
+  primary_muscles muscle[],
+  secondary_muscles muscle[],
+  force forceType,
+  level levelType NOT NULL,
+  mechanic mechanicType,
+  equipment equipmentType,
+  category categoryType NOT NULL,
+  instructions Text[],
+  description Text,
+  tips Text[],
+  date_created timestamptz NOT NULL DEFAULT now(),
+  date_updated timestamptz NOT NULL DEFAULT statement_timestamp(),
+  PRIMARY KEY ("id"),
+  UNIQUE ("name")
+);
+
+DROP TABLE IF EXISTS users;
+CREATE TABLE users (
+	id            SERIAL PRIMARY KEY,
+	email         VARCHAR(200) NOT NULL,
+	password      VARCHAR(200) NOT NULL,
+  token         VARCHAR(100) DEFAULT NULL,
+  name          VARCHAR(300) DEFAULT NULL,
+  created_at    timestamptz NOT NULL DEFAULT now(),
+  updated_at    timestamptz NOT NULL DEFAULT statement_timestamp(),
+	UNIQUE (email)
+);
+
+CREATE TABLE plans(
+  id            SERIAL PRIMARY KEY,
+  name          VARCHAR(200) NOT NULL,
+  -- user_id       INT NOT NULL,
+  created_at    timestamptz NOT NULL DEFAULT now(),
+  updated_at    timestamptz NOT NULL DEFAULT statement_timestamp(),
+);
+
+CREATE TABLE plan_exercises(
+  id            SERIAL PRIMARY KEY,
+  -- user_id       INT NOT NULL,
+  plan_id       INT NOT NULL,
+  exercise_id   INT NOT NULL,
+  CONSTRAINT    fk_plan FOREIGN KEY(plan_id) REFERENCES plans(id),
+  CONSTRAINT    fk_exercise FOREIGN KEY(exercise_id) REFERENCES exercises(id),
+  -- CONSTRAINT    fk_users FOREIGN KEY(user_id) REFERENCES users(id),
+);
+
+CREATE TABLE workouts(
+  id            SERIAL PRIMARY KEY,
+  plan_id       INT DEFAULT NULL,
+  created_at    timestamptz NOT NULL DEFAULT now(),
+  updated_at    timestamptz NOT NULL DEFAULT statement_timestamp(),
+  CONSTRAINT    fk_plan FOREIGN KEY(plan_id) REFERENCES plans(id),
+);
+
+CREATE TABLE workout_exercises(
+  id            SERIAL PRIMARY KEY,
+  workout_id    INT NOT NULL,
+  exercise_id   INT NOT NULL,
+  created_at    timestamptz NOT NULL DEFAULT now(),
+  updated_at    timestamptz NOT NULL DEFAULT statement_timestamp(),
+  CONSTRAINT    fk_workout FOREIGN KEY(workout_id) REFERENCES workouts(id),
+  CONSTRAINT    fk_exercise FOREIGN KEY(exercise_id) REFERENCES exercises(id),
+);
+
+CREATE TABLE workout_set(
+  id            SERIAL PRIMARY KEY,
+  workout_id    INT NOT NULL,
+  exercise_id   INT NOT NULL,
+  weight        INT NOT NULL,
+  reps          INT NOT NULL,
+  created_at    timestamptz NOT NULL DEFAULT now(),
+  updated_at    timestamptz NOT NULL DEFAULT statement_timestamp(),
+  CONSTRAINT    fk_workout FOREIGN KEY(workout_id) REFERENCES workouts(id),
+  CONSTRAINT    fk_exercise FOREIGN KEY(exercise_id) REFERENCES exercises(id),
+)
